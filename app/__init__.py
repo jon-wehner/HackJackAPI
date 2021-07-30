0c8fb0a0a75a641b0cee682140c291cd76796beb
Jon Wehner <jonjwehner@gmail.com>
2021-07-29 23:15:37 -0700

    setup MongoDB

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from pymongo import MongoClient
+from pprint import pprint
+import os
+
 
 app = Flask(__name__)
+
+client = MongoClient(os.environ.get('DATABASE_URL'))
 
 
 @app.route('/')
