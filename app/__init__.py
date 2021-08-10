ab4cbbaad04c3117a78447ed561bf02f0f07e810
Jon Wehner <jonjwehner@gmail.com>
2021-08-10 01:39:34 -0700

    add user function created

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,20 @@
 import os
-import bcrypt
 from flask import Flask
-from flask_jwt_extended import (create_access_token,
-                                get_jwt_identity, jwt_required, JWTManager)
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
-from pprint import pprint
-from .models import get_db
-
-app = Flask(__name__)
-
-bcrypt = Bcrypt(app)
-CORS(app)
-
-app.config['JWT_SECRET_KEY'] = os.environ.get('JWTSECRETKEY')
-jwt = JWTManager(app)
-
-pprint(get_db())
 
 
-@app.route('/')
-def hello_world():
-    return {"message": "Hello World"}
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    app.config['JWT_SECRET_KEY'] = os.environ.get('JWTSECRETKEY')
+    app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
+    app.config['DATABASE_NAME'] = os.environ.get('DATABASE_NAME')
+    app.config['BCRYPT'] = Bcrypt(app)
+
+    jwt = JWTManager(app)
+
+    CORS(app)
+
+    return app
---
+++ app/db.py
@@ -0,0 +1,32 @@
+from flask import current_app, g
+from werkzeug.local import LocalProxy
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+
+def get_db():
+    db = getattr(g, "_database", None)
+
+    DB_URI = current_app.config['DATABASE_URL']
+    DB_NAME = current_app.config['DATBASE_NAME']
+
+    if db is None:
+        db = g._database = MongoClient(DB_URI)[DB_NAME]
+
+    return db
+
+
+db = LocalProxy(get_db)
+
+
+def add_user(name, email, hashedpassword):
+    try:
+        db.users.insert_one({
+            "name": name,
+            "email": email,
+            "password": hashedpassword,
+            "chips": 100
+        }, {"writeConcern": "majority"})
+        return {"success": "User Registered"}
+    except DuplicateKeyError:
+        return {"error": "Users Must have unique Email addresses"}
