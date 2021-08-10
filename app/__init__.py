d7eebc8606ee5493ee740bcd995b88120825c583
Jon Wehner <jonjwehner@gmail.com>
2021-08-09 20:56:26 -0700

    switched to jwt

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
+import os
 from flask import Flask
-from flask_login import LoginManager
+from flask_jwt_extended import (create_access_token,
+                                get_jwt_identity, jwt_required, JWTManager)
 from pymongo import MongoClient
 from pprint import pprint
-import os
 
 
 app = Flask(__name__)
-login_manager = LoginManager(app)
+
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWTSECRETKEY')
+jwt = JWTManager(app)
 
 client = MongoClient(os.environ.get('DATABASE_URL'))
 db = client.admin
