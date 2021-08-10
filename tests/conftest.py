678053cd0935be73794d69c5fe6a086338bb5530
Jon Wehner <jonjwehner@gmail.com>
2021-08-10 03:43:58 -0700

    app is currently timing out trying to connect to localhost during testing instead of atlas

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from app import create_app
 from app.db import get_db
-from werkzeug.local import LocalProxy
+from contextlib import contextmanager
+from flask import appcontext_pushed, g
 
 import os
 
@@ -12,6 +13,8 @@ def client():
     app.config['JWT_SECRET_KEY'] = os.environ.get('TEST_SECRET_KEY')
     app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
     app.config['DATABASE_NAME'] = os.environ.get('TEST_DATABASE_NAME')
+    app.config['TESTING'] = True
     with app.test_client() as client:
         with app.app_context():
+            get_db()
             yield client
