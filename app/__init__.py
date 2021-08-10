8fbb5eeffdf69786ff516edeee168d5d5ab988e1
Jon Wehner <jonjwehner@gmail.com>
2021-08-09 19:22:48 -0700

    added flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 from pymongo import MongoClient
 from pprint import pprint
 import os
 
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 client = MongoClient(os.environ.get('DATABASE_URL'))
 db = client.admin
